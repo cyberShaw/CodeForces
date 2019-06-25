4cf86293a3d51c0a270a5b5733f39d6fb0f82963
Saiharsha B <34317373+thewhitewolf17@users.noreply.github.com>
2019-06-25 23:51:12 +0530

    Add 122a

--- 122a-lucky-division.py
+++ 122a-lucky-division.py
@@ -1,11 +1,26 @@
-x = int(input())
-if(x%4==0 or x%7==0):
-    print("YES")
-    exit()
-else:
-    x = str(x)
-    for i in x:
-        if(i!='7' and i!='4'):
-            print("NO")
-            exit()
-    print("YES")
+def solve(n):
+
+    if isLucky(n):
+        return "YES"
+
+    for x in xrange(2,n/2):
+        if n % x == 0 and (isLucky(x) or isLucky(n/x)):
+            return "YES"
+
+    return "NO"
+
+
+def isLucky(n):
+
+    while n > 0:
+        digit = n % 10
+        if digit != 4 and digit != 7:
+            return False
+        n /= 10
+
+    return True
+
+
+if __name__ == "__main__":
+    n = int(raw_input())
+    print solve(n)
